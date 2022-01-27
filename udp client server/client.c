b4bedbda4378b0b5ebc06dc878fa2c0149d7d194
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-01-27 14:09:59 +0530

    Update client.c

--- udp client server/client.c
+++ udp client server/client.c
@@ -10,10 +10,8 @@
 int main(){
   int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0); 
   socklen_t addressLength;
-  
   char sendMessage[MAXLINE],recvMessage[MAXLINE];
-  printf("\nEnter message :");
-  
+  printf("\nEnter message :");  
   fgets(sendMessage,sizeof(sendMessage),stdin);
   struct sockaddr_in serverAddress;
   serverAddress.sin_family = AF_INET;
@@ -23,8 +21,6 @@ int main(){
   connect(serverDescriptor,(struct sockaddr*)&serverAddress,addressLength);
   sendto(serverDescriptor,sendMessage,MAXLINE,0,(struct sockaddr*)&serverAddress,addressLength);
   recvfrom(serverDescriptor,recvMessage,MAXLINE,0,NULL,NULL);
- 
   printf("\nServer's Echo : %s\n",recvMessage);
-
   return 0;
 }

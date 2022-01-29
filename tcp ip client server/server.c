353e23599f3b5abc182d622719294f79de352a57
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-01-29 14:35:40 +0530

    Update server.c

--- tcp ip client server/server.c
+++ tcp ip client server/server.c
@@ -8,12 +8,14 @@
 int main(){
   char serverMessage[256] = "You have a missed call from server\n";
   int socketDescriptor = socket(AF_INET,SOCK_STREAM,0);
+
   struct sockaddr_in serverAddress;
   serverAddress.sin_family = AF_INET;
   serverAddress.sin_port = htons(9002);
   serverAddress.sin_addr.s_addr = INADDR_ANY;
   bind(socketDescriptor,(struct sockaddr*) &serverAddress,sizeof(serverAddress));
   listen(socketDescriptor,5);
+
   int client_socket = accept(socketDescriptor, NULL, NULL);
   send(client_socket,serverMessage,sizeof(serverMessage),0);
   close(socketDescriptor);

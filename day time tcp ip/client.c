b138af9247c529fb4f1bf8ab9c01aa6190764680
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-02-09 14:51:33 +0530

    Update client.c

--- day time tcp ip/client.c
+++ day time tcp ip/client.c
@@ -14,9 +14,7 @@ int main(){
   serverAddress.sin_addr.s_addr = INADDR_ANY;
   serverAddress.sin_port = htons(PORT);
 connect(socket_descriptor, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
-  
 recv(socket_descriptor,serverResponse,MAXLINE-1,0);
-
   printf("\nTIME FROM SERVER %s\n",serverResponse);
 
   return 0;
